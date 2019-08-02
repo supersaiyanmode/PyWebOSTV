e73438c4de9817bb98d8aa300a59cdaa218771c7
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2019-08-02 15:11:57 -0700

    Bump the version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name='pywebostv',
-    version='0.8.3',
+    version='0.8.4',
     url='https://github.com/supersaiyanmode/PyWebOSTV',
     author='Srivatsan Iyer',
     author_email='supersaiyanmode.rox@gmail.com',
